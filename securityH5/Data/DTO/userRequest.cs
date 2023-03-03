273a8bccdc789e0e1eb6c8323aeb119303223665
krytan <krytanx@gmail.com>
2023-03-03 09:46:11 +0100

    Latest version

---
+++ securityH5/Data/DTO/userRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace securityH5.Data.DTO
+{
+    public class userRequest
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Message { get; set; }
+        public byte[]? AccountHash { get; set; }
+        public byte[]? Accountsalt { get; set; }
+
+    }
+}
+
